044e1ff81d838c33c55686134b59015868b74de8
Kelvin Rodrigues <kelvin.rodrigues@ebinterchange.com.br>
2025-04-06 09:34:15 -0300

    Finish test unitario

--- exercicio5ApiCadastroVeiculos/src/test/java/com/exercicio5apicadastroveiculos/Exercicio5ApiCadastroVeiculosApplicationTests.java
+++ exercicio5ApiCadastroVeiculos/src/test/java/com/exercicio5apicadastroveiculos/Exercicio5ApiCadastroVeiculosApplicationTests.java
@@ -1,13 +1,20 @@
 package com.exercicio5apicadastroveiculos;
 
+import com.exercicio5apicadastroveiculos.domain.repository.CarRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Exercicio5ApiCadastroVeiculosApplicationTests {
+
+    @MockitoBean
+    private CarRepository carRepository;
 
     @Test
     void contextLoads() {
+        System.out.println("Application started with mocks.");
     }
-
 }
